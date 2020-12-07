2c399dc9fefd2392ee195fbc61fb037a9c6a1533
Shana <69315804+shanalfe@users.noreply.github.com>
2020-12-07 09:03:15 +0000

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function openLeftMenu() {
+    
+  function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
